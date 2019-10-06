a4acda155f2fcd55eaf5ee151264af945230f8b5
lahiru-j <lahirujayathilake@gmail.com>
2019-10-06 21:35:15 +0530

    entity classes for role and user

---
+++ src/main/java/com/example/productcatalog/entity/Role.java
@@ -0,0 +1,48 @@
+package com.example.productcatalog.entity;
+
+import javax.persistence.*;
+
+/**
+ * Entity class for Role
+ *
+ * @author Lahiru Jayathilake
+ * @since 0.0.1-SNAPSHOT
+ */
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "description")
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
